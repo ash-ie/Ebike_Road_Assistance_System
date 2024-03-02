e26022abfb0b6a23f6b992ec809a19f833255628
ash-ie <asw097397@gmail.com>
2024-03-02 22:10:35 +0530

    template work completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('sign-in/',views.login_view,name='sign-in'),
+    path('sign-up/',views.register,name='sign-up'),
+    path('admin_home/',views.admin_home,name='admin_home'),
+    path('customer_home/',views.customer_home,name='customer_home'),
+    path('worker_home/',views.worker_home,name='worker_home'),
 ]
